19cfae1282980f9f03948c2c02bf1260d3a7d310
성주 <think8521@naver.com>
2023-07-09 00:44:11 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] common) {
+        
+        int answer = 0;
+        if (common[2] - common[1] == common[1] - common[0]) {
+            answer = common[common.length-1] + (common[2] - common[1]);
+        } else {
+            answer = common[common.length-1] * (common[2] / common[1]);
+        }
+            
+            
+        return answer;
+    }
+}
