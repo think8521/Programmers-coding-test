57d079ae2fc3a9b7c772e2c0232ff078db213661
성주 <think8521@naver.com>
2023-07-11 19:15:39 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.28 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int a = 0;
+        int b = 0;
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                    if (i != j && Math.min(lines[i][1], lines[j][1]) > Math.max(lines[i][0], lines[j][0])) {
+                        answer += Math.min(lines[i][1], lines[j][1]) - Math.max(lines[j][0], lines[i][0]);
+                    }
+                for (int k = 0; k < 3; k++) {
+                    if (i != j && j != k && k != i) {
+                        if (lines[k][1] > lines[i][0] && lines[k][1] > lines[j][0] &&
+                           lines[k][1] <= lines[i][1] && lines[k][1] <= lines[j][1] ) {
+                            a = lines[k][1] - Math.max(lines[i][0], lines[j][0]);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+        System.out.println(a);
+        answer = answer/2 - 2*a;
+        return answer;
+    }
+}
