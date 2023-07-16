1073a3c00301fb54b0dbf99d77ec0cedafcea6bd
성주 <think8521@naver.com>
2023-07-16 11:55:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long num2 = (long) num;
+        
+        while (num2 > 1) {
+            if (num2%2==0) {
+                num2 = num2/2;
+                answer++;
+            } else {
+                num2 = num2 * 3 + 1;
+                answer++;
+            }
+            if (answer == 500) {
+                answer = -1;
+                return answer;
+            }
+        }
+        
+        // if (answer >= 500) {
+        //     answer = -1;
+        // }
+        
+        return answer;
+    }
+}
