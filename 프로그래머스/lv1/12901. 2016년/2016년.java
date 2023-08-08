e4d4a8cfd7789523fa46376a7f9f1b09c5980eac
성주 <think8521@naver.com>
2023-08-08 15:42:55 +0900

    [level 1] Title: 2016년, Time: 19.74 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,12 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+class Solution {
+    public String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek dow = date.getDayOfWeek();
+        String answer = dow.getDisplayName(TextStyle.SHORT, Locale.getDefault()).toUpperCase();
+        return answer;
+    }
+}
