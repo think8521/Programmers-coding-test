984eb8f5a3fe532637af5df4640c17afc7d37c72
성주 <think8521@naver.com>
2023-08-23 18:54:45 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.85 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        int[] numbers = new int[number];
+        
+        for (int i = 0; i < number; i++) {
+            numbers[i] = i+1;
+        }
+        
+        for (int num : numbers) {
+            int ys = 0;
+            // if (num == 1) {
+            //     ys = 1;
+            // } else {
+                for (int i = 1; i*i <= num; i++) {
+                    if (num % i == 0) {
+                        int temp = num/i;
+                        if (temp == i) {
+                            ys++;
+                        } else {
+                            ys += 2;
+                        }
+                    }
+                }
+            // }
+            if (ys > limit) {
+                ys = power;
+            }
+            list.add(ys);
+        }
+        
+        for (int steel : list) {
+            answer += steel;
+        }
+        return answer;
+    }
+}
