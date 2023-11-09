63f43af9b759ce43b62a8301128091b724dbe574
성주 <think8521@naver.com>
2023-11-09 22:09:51 +0900

    [level unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        myString = myString.toLowerCase();
+        myString = myString.replace("a", "A");
+        String answer = myString;
+        return answer;
+    }
+}
