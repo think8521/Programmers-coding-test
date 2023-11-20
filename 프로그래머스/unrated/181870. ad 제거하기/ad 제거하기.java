d948975355afc04d53846b29555875771acd18ea
성주 <think8521@naver.com>
2023-11-21 00:13:54 +0900

    [level unrated] Title: ad 제거하기, Time: 0.68 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> arr = new ArrayList<>();
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+              arr.add(str);  
+            }
+        }
+        String[] answer = arr.toArray(new String[0]);
+        return answer;
+    }
+}
